53ec72b359b7e93632b80305e24f6fcf4895c12b
Ivan Filenko <ivan.filenko@shiphawk.com>
2019-09-05 17:06:04 +0300

    Dont trace requests in test env

--- lib/active_utils/connection.rb
+++ lib/active_utils/connection.rb
@@ -1,6 +1,5 @@
 require 'uri'
 require 'net/http'
-# require 'net/http/middleware'
 require 'net/https'
 require 'benchmark'
 
@@ -58,7 +57,7 @@ module ActiveUtils
           info "connection_http_method=#{method.to_s.upcase} connection_uri=#{endpoint}", tag
 
           connection = Faraday.new(endpoint) do |faraday|
-            faraday.use(ZipkinTracer::FaradayHandler, endpoint.host) unless ::Rails.env.test?
+            faraday.use(ZipkinTracer::FaradayHandler, endpoint.host) unless ENV['RAILS_ENV'] == 'test'
             faraday.use :extended_logging, logger: logger if logger.present?
             faraday.request :timer
             faraday.adapter :httpclient
